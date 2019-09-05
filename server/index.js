726ee2a3609aed313d4b4a998170f6e967ceea6e
Chet Hill <chetdhill@gmail.com>
2019-09-05 11:51:48 -0400

    Connect to Public Folder

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ app.set('view engine', 'pug');
 // add views folder
 app.set('views', path.join(__dirname, './views') );
 
+// load the public assets folder
+app.use(express.static('public'));
+
+// get the current year
+app.use((req, res, next) => {
+    const date = new Date();
+    res.locals.currentYear = date.getFullYear();
+    return next();
+});
+
 // listen for the home page
 app.use('/', routes() );
 
